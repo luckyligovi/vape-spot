8a00e9a80eeffb2b6ddb3b845ed550ba69808f57
luckyligovi <lucky.luvihi@student.moringaschool.com>
2023-12-11 15:35:31 +0300

    changes on navbar

--- vape-spot/src/components/Navbar.jsx
+++ vape-spot/src/components/Navbar.jsx
@@ -7,68 +7,81 @@ import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
-// import Button from "@mui/material/Button"; // Import Button directly
 import LocalPhoneRoundedIcon from "@mui/icons-material/LocalPhoneRounded";
 import DirectionsRoundedIcon from "@mui/icons-material/DirectionsRounded";
 
 function Navbar() {
-
-
   return (
     <div className="navBar">
       <Box sx={{ flexGrow: 1 }}>
         <AppBar
-          position="static"
-          sx={{ height: 80, backgroundColor: "transparent", color: "inherit"}}
+          position="sticky"
+          sx={{
+            height: 80,
+            backgroundColor: "transparent",
+            color: "inherit",
+            // borderBottom: "1px solid #eee",
+          }}
         >
-          <Toolbar variant="dense">
+          <Toolbar variant="dense" sx={{ marginLeft: "250px", marginRight: "250px" }}>
             <IconButton edge="start" aria-label="menu" sx={{ mr: 2 }}>
               <MenuIcon sx={{ color: "white" }} />
             </IconButton>
+
             <IconButton
               edge="start"
               color="inherit"
               aria-label="home"
-              sx={{ mr: 2, marginBottom: 0.5 }}
+              sx={{
+                mr: 2,
+                marginBottom: 0.5,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
             >
-              <Typography variant="h5" component="div" textAlign="center">
+              <HomeRoundedIcon sx={{ fontSize: 32 }} />
+              <Typography
+                variant="body2"
+                component="div"
+                textAlign="center"
+                sx={{ marginTop: 1, color: "white" }}
+              >
                 <Link
                   to="/"
                   style={{ textDecoration: "none", color: "inherit" }}
                 >
-                  <HomeRoundedIcon />
-                  home
+                  Home
                 </Link>
               </Typography>
             </IconButton>
 
-            <h1 style={{marginBottom:30}}>vape-spot.ke</h1>
+            <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+              <Link
+                to="/"
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                vape-spot.ke
+              </Link>
+            </Typography>
 
             <div className="rightContent-navBar">
-              {/* <Button sx={{ margin: 1 }} color="inherit">
-                <Link
-                  to="/about"
-                  style={{ textDecoration: "none", color: "inherit" }}
-                >
-                  About
-                </Link>
-              </Button> */}
               <IconButton sx={{ margin: 3 }} color="inherit">
                 <Link
                   to="/services"
                   style={{ textDecoration: "none", color: "inherit" }}
                 >
-                  <DirectionsRoundedIcon />
+                  <DirectionsRoundedIcon sx={{ fontSize: 28 }} />
                   Get Directions
                 </Link>
               </IconButton>
-              <IconButton sx={{ margin: 1 }} color="inherit">
+              <IconButton sx={{ margin: 1,  }} color="inherit">
                 <Link
                   to="/contact"
-                  style={{ textDecoration: "none", color: "inherit" }}
+                  style={{ textDecoration: "none", color: "inherit", display: "flex", alignItems: "center" }}
                 >
-                  <LocalPhoneRoundedIcon />
-                  Contact
+                  <LocalPhoneRoundedIcon sx={{ fontSize: 20, }} />
+                  <span style={{ marginLeft: "5px" }}>+254721475143</span>
                 </Link>
               </IconButton>
             </div>
